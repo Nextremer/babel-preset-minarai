5e49215ccfaa0ccd2c14e66a3ef4e34257906a04
k-tada <white.rv0925@gmail.com>
2018-04-19 11:23:46 +0900

    fix bug

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 'use strict';
 
-module.exports = {
-  presets: [
-    [require('babel-preset-env'), {
-      targets: { browsers: ['last 2 versions'] },
-      modules: false
-    }],
-    require('babel-preset-react'),
-    require('babel-preset-stage-0')
-  ],
-  plugins: [
-    require('babel-plugin-transform-decorators-legacy'),
-    [require('babel-plugin-root-import'), [{
-      rootPathPrefix: '~',
-      rootPathSuffix: 'src/js/'
-    }]],
-    require('babel-plugin-import-glob')
-  ]
+module.exports = function buildMinaraiPreset(context, options) {
+  var modules = (options && (typeof options.modules === 'boolean' || typeof options.modules === 'string')) ? options.modules : false;
+  return {
+    presets: [
+      [require('babel-preset-env').default, {
+        targets: { browsers: ['last 2 versions'] },
+        modules: modules
+      }],
+      require.resolve('babel-preset-react'),
+      require.resolve('babel-preset-stage-0')
+    ],
+    plugins: [
+      require.resolve('babel-plugin-transform-decorators-legacy'),
+      [require('babel-plugin-root-import').default, [{
+        rootPathPrefix: '~',
+        rootPathSuffix: 'src/js/'
+      }]],
+      require.resolve('babel-plugin-import-glob')
+    ]
+  };
 };
